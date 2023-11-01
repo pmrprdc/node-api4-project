fc39598f175de55b1fc1356947d7f057afefaebf
Pamir Perdeci <pamirperdeci@gmail.com>
2023-11-01 19:39:30 -0400

    able to post new users and all wrong endpoints are caught with and responded with a custom message

--- index.js
+++ index.js
@@ -13,9 +13,37 @@ const initialUsers = [
 ]
 
 
+
+
 server.get('/users',(req,res)=>{
     res.json(initialUsers)
 })
+
+
+server.post('/users', (req, res) => {
+    const { username, password } = req.body;
+    
+    // Check for missing username or password
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Bad Request, Please provide a username and password"
+        });
+    } else {
+        // Corrected the spelling of 'passaword' to 'password'
+        initialUsers.push({ username, password });
+        return res.status(201).json({
+            message: `User ${username} has successfully been added`
+        });
+    }
+});
+
+
+
+server.use((req, res) => {
+    res.status(404).json({ message: "Wrong endpoint" });
+});
+
+
 
 server.listen(port,()=>{
     console.log(`Listening on port ${port}`)
